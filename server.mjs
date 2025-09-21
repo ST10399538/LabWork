d0a1c9ca8fbd463f1aa903e603aff226599bb204
Inge <dafelinge@gmail.com>
2025-09-21 21:28:26 +0200

    routing

--- server.mjs
+++ server.mjs
@@ -1,17 +1,44 @@
 import https from "https";
+import http from "http";
 import fs from "fs";
+import posts from "./routes/post.mjs";
+import users from "./routes/user.mjs";
 import app from "./app.mjs";
 import express from "express";
+import cors from "cors"
 //set the port
 const PORT = 3000;
 const app = express();
-const urlprefix = '/api';
 
+//const urlprefix = '/api';
 
-const server = https.createServer({
+const options = {
     key: fs.readFileSync('keys/privatekey.pem'),
     cert: fs.readFileSync('keys/certificate.pem')
-},app)
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use((reg, res,next) =>
+{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','*');
+    res.setHeader('Access-Control-Allow-Methods','*');
+})
+
+app.use("/post", posts);
+app.route("/post", posts);
+app.use("/user, users");
+app.route("/user", users);
+
+let server = https.createServer(options,app)
+console.log(PORT)
+server.listen(PORT);
+// const server = https.createServer({
+//     key: fs.readFileSync('keys/privatekey.pem'),
+//     cert: fs.readFileSync('keys/certificate.pem')
+// },app)
 
 // app.use(express.json());
 
