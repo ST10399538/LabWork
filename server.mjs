ef66b9ef194430fc3e1de41a7a62055c66ecf71a
Inge <dafelinge@gmail.com>
2025-09-21 21:13:41 +0200

    added mongodb connectionstring

--- server.mjs
+++ server.mjs
@@ -7,35 +7,42 @@ const PORT = 3000;
 const app = express();
 const urlprefix = '/api';
 
-app.use(express.json());
 
-app.get(urlprefix + '/', (req, res) => {
-    res.send('I am finally figuring this out, no more crying')
-})
+const server = https.createServer({
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+},app)
 
-app.get(urlprefix + '/orders', (req,res) => {
-    const orders = [
-        {
-            id: "1",
-            name: "Orange"
-        },
-        {
-            id: "2",
-            name: "Apple"
-        },
-        {
-            id: "3",
-            name: "Pear"
-        }
-    ]
-    res.json(
-        {
-            message: "Fruits",
-            orders: orders
-        }
-    )
-})
+// app.use(express.json());
+
+// app.get(urlprefix + '/', (req, res) => {
+//     res.send('I am finally figuring this out, no more crying')
+// })
+
+// app.get(urlprefix + '/orders', (req,res) => {
+//     const orders = [
+//         {
+//             id: "1",
+//             name: "Orange"
+//         },
+//         {
+//             id: "2",
+//             name: "Apple"
+//         },
+//         {
+//             id: "3",
+//             name: "Pear"
+//         }
+//     ]
+//     res.json(
+//         {
+//             message: "Fruits",
+//             orders: orders
+//         }
+//     )
+// })
 
 
 
-app.listen(PORT)
+// app.listen(PORT)
+server.listen(PORT)
